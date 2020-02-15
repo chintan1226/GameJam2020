2dd8bb8482b969c452e4b0558b70865887580d31
Chintan <chintanpatel1226@gmail.com>
2020-02-15 11:03:26 -0600

    big push

---
+++ GameJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public so we can change it from the inspector
+    public float speed;
+    
+    //How much I want the player's position to change
+    private Rigidbody2D _myRigidbody;
+
+    //player's positioning
+    private Vector3 change;
+
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myRigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxis("Horizontal");
+        change.y = Input.GetAxis("Vertical");
+        UpdateAnimationAndMove();
+    }
+
+    void MoveCharacter()
+    {
+        _myRigidbody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            MoveCharacter();
+            _animator.SetFloat("moveX", change.x);
+            _animator.SetFloat("moveY", change.y);
+            _animator.SetBool("moving", true);
+        }
+        else
+        {
+            _animator.SetBool("moving", false);
+        }
+    }
+}
